564ef41b46711110b6c9f1afe237b6fadf2dc3ce
AenuHub <jesuskane92@gmail.com>
2025-01-23 17:35:52 +0300

    add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+let choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
